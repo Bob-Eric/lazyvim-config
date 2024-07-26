2af8863584cd77823a16aa0d152f72c829a8ac1a
chy <1274653465@qq.com>
2024-07-26 20:50:03 +0800

    Change startup theme

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
---if vim.g.vscode then
--- VSCode extension
---else
--- ordinary Neovim
+if vim.fn.has("win32") == 1 then
+  vim.o.shell = "powershell"
+end
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
---end
