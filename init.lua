344a591ef7e6bdc3df127ace6f6ffc30cdab8997
chy <1274653465@qq.com>
2024-08-01 01:03:37 +0800

    lzgit - 2024-07-31T17:03:37.028Z

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 if vim.fn.has("win32") == 1 then
   vim.o.shell = "powershell"
 end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.vscode then
+    local map = LazyVim.safe_keymap_set
+    -- Deal with vscode buffers
+    -- Go to next buffer
+    map("n", "L", "<Cmd>lua require('vscode').call('workbench.action.nextEditorInGroup')<CR>", { desc = "Next Buffer" })
+    map("x", "L", "<Cmd>lua require('vscode').call('workbench.action.nextEditorInGroup')<CR>", { desc = "Next Buffer" })
+    map("n", "]b", "<Cmd>lua require('vscode').call('workbench.action.nextEditorInGroup')<CR>", { desc = "Next Buffer" })
+    map("x", "]b", "<Cmd>lua require('vscode').call('workbench.action.nextEditorInGroup')<CR>", { desc = "Next Buffer" })
+
+    -- Go to previous buffer
+    map("n", "H", "<Cmd>lua require('vscode').call('workbench.action.previousEditorInGroup')<CR>",
+        { desc = "Previous Buffer" })
+    map("x", "H", "<Cmd>lua require('vscode').call('workbench.action.previousEditorInGroup')<CR>",
+        { desc = "Previous Buffer" })
+    map("n", "[b", "<Cmd>lua require('vscode').call('workbench.action.previousEditorInGroup')<CR>",
+        { desc = "Previous Buffer" })
+    map("x", "[b", "<Cmd>lua require('vscode').call('workbench.action.previousEditorInGroup')<CR>",
+        { desc = "Previous Buffer" })
+
+    -- Delete buffer
+    map("n", "<leader>bd", "<Cmd>lua require('vscode').call('workbench.action.closeActiveEditor')<CR>")
+    map("x", "<leader>bd", "<Cmd>lua require('vscode').call('workbench.action.closeActiveEditor')<CR>")
+
+    map("n", "<leader>bo", "<Cmd>lua require('vscode').call('workbench.action.closeOtherEditors')<CR>")
+    map("x", "<leader>bo", "<Cmd>lua require('vscode').call('workbench.action.closeOtherEditors')<CR>")
+
+    -- Toggle sidebar
+    map("n", "<leader>e", "<Cmd>lua require('vscode').call('workbench.action.toggleSidebarVisibility')<CR>")
+    map("x", "<leader>e", "<Cmd>lua require('vscode').call('workbench.action.toggleSidebarVisibility')<CR>")
+end
