977cb74c1ae1839a7c281bfaf1bfdf4bf3cdc101
chy <1274653465@qq.com>
2024-08-01 11:14:54 +0800

    添加vscode模式快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,4 +30,29 @@ if vim.g.vscode then
     -- Toggle sidebar
     map("n", "<leader>e", "<Cmd>lua require('vscode').call('workbench.action.toggleSidebarVisibility')<CR>")
     map("x", "<leader>e", "<Cmd>lua require('vscode').call('workbench.action.toggleSidebarVisibility')<CR>")
+
+    -- Lazygit
+    map("n", "<leader>gg", "<Cmd>lua require('vscode').call('lazygit.openLazygit')<CR>")
+    map("x", "<leader>gg", "<Cmd>lua require('vscode').call('lazygit.openLazygit')<CR>")
+
+    -- Clipboard
+    -- map("n", "<leader>y", "<Cmd>lua require('vscode').call('multiclip.list')<CR>")
+    -- map("x", "<leader>y", "<Cmd>lua require('vscode').call('multiclip.list')<CR>")
+
+    -- Copilot chat
+    map("n", "<leader>aa", "<Cmd>lua require('vscode').call('workbench.panel.chat.view.copilot.focus')<CR>")
+    map("x", "<leader>aa", "<Cmd>lua require('vscode').call('workbench.panel.chat.view.copilot.focus')<CR>")
+
+    -- Collapse
+    map("n", "zc", "<Cmd>lua require('vscode').call('editor.fold')<CR>")
+    map("x", "zc", "<Cmd>lua require('vscode').call('editor.fold')<CR>")
+
+    map("n", "zo", "<Cmd>lua require('vscode').call('editor.unfold')<CR>")
+    map("x", "zo", "<Cmd>lua require('vscode').call('editor.unfold')<CR>")
+
+    map("n", "zM", "<Cmd>lua require('vscode').call('editor.foldAll')<CR>")
+    map("x", "zM", "<Cmd>lua require('vscode').call('editor.foldAll')<CR>")
+
+    map("n", "zR", "<Cmd>lua require('vscode').call('editor.unfoldAll')<CR>")
+    map("x", "zR", "<Cmd>lua require('vscode').call('editor.unfoldAll')<CR>")
 end
