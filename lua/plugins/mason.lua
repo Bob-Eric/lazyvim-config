cc2ad601561d7f2bf1ceac8d0a4dff24008b4521
chy <1274653465@qq.com>
2025-09-29 08:13:17 +0800

    chore: updated mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
   {
 
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     build = ":MasonUpdate",
