ff5eb768e05f98f7039ebd26c3d5b0421d9205a8
chy <1274653465@qq.com>
2026-01-10 00:27:41 +0800

    chore: disable spell check

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ if vim.fn.has("win32") == 1 then
   vim.o.shell = "powershell"
 end
 
+-- disable spell
+vim.o.spell = false
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
