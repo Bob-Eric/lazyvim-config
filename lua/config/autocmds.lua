b020a5d961b84fef79af37ad9f4f2ab67bda6a3c
DESKTOP-E4VIK7O <1274653465@qq.com>
2026-01-12 17:07:12 +0800

    chore: disable spell by default

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+-- disable spell
+vim.o.spell = false
+vim.opt_local.spell = false
