54f263032a25bfb33e4cbc1502181f793157da53
DESKTOP-E4VIK7O <1274653465@qq.com>
2026-01-14 10:22:27 +0800

    feat: add jieba to support chinese movement

---
+++ lua/plugins/jieba.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "noearc/jieba.nvim",
+    vscode = true,
+    dependencies = { "noearc/jieba-lua" },
+    opts = {},
+  },
+  {
+    "noearc/jieba-lua",
+    vscode = true,
+  },
+}
