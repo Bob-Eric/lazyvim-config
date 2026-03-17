a03eb8ee7d3ceaf78a5c2e76fbb4f4eda8633c5c
chy <1274653465@qq.com>
2026-03-17 16:01:41 +0800

    changed jieba and im-select config

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -3,7 +3,9 @@ return {
     "keaising/im-select.nvim",
     vscode = true,
     config = function()
-      require("im_select").setup({})
+      require("im_select").setup({
+        set_default_events = { "InsertLeave" },
+      })
     end,
   },
 }
--- lua/plugins/jieba.lua
+++ lua/plugins/jieba.lua
@@ -1,11 +1,12 @@
 return {
   {
     "kkew3/jieba.vim",
-    tag = "v1.0.5",
+    tag = "v1.0.6",
     vscode = true,
     init = function()
       vim.g.jieba_vim_lazy = 1
       vim.g.jieba_vim_keymap = 1
     end,
+    build = "./build.ps1",
   },
 }
